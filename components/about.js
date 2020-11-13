f8e22bbc2a15eea5fb4dc4e31a307aae887e9513
Jonathan King <jonpking@gmail.com>
2020-11-12 23:16:01 -0700

    updated layout, content, styling

--- components/about.js
+++ components/about.js
@@ -2,19 +2,35 @@ import styles from "../styles/About.module.css"
 
 export default function About() {
     return (
-        <div className={styles.aboutHeaders}>
-            <h1>
-                Jonathan King
-            </h1>
-            <h2>
-                Full-stack web developer with a passion for user friendly designs
-            </h2>
-            <button>
-                View my work
-            </button>
-            <button>
-                Get in touch
-            </button>
+        <div className={styles.aboutSection}>
+            <div className={styles.aboutHeadersAndImageDiv}>
+                <div className={styles.aboutHeaders}>
+                    <h1>
+                        Jonathan King
+                    </h1>
+                    <h2>
+                        Full-stack web developer with a passion
+                        <br />
+                        for user friendly designs
+                    </h2>
+                </div>
+                <img src="/images/self_cropped.png" className={styles.selfImage}></img>
+            </div>
+            <div className={styles.aboutButtonsAndTextDiv}>
+                <div className={styles.aboutButtons}>
+                    <button className={styles.aboutButton}>View my work</button>
+                    <button className={styles.aboutButton}>Get in touch</button>
+                </div>
+                <div>
+                    <p>Life long learner, skilled problem solver,
+                    <br />
+                    communication and customer service expert,
+                    <br />
+                    experienced team player, strong work ethic,
+                    <br />
+                    and enthusiastic gamer of nearly every flavor.</p>
+                </div>
+            </div>
         </div>
     )
 }
