be0866b5d6d33202a8398b5872c44ed1381b586a
Jonathan King <jonpking@gmail.com>
2020-11-12 20:21:56 -0700

    started building out page and applying basic styling

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head"
+import styles from "../styles/Home.module.css"
+import Navbar from "../components/navbar.js"
+import About from "../components/about.js"
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Jonathan King</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        
+      <main className={styles.pageContainer}>
+        <Navbar />
+        <About />
       </main>
 
     </div>
