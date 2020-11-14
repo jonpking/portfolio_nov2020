b67c456f9e04aa7c5f1d6b74296f82c0fcf0525f
Jonathan King <jonpking@gmail.com>
2020-11-13 21:39:36 -0700

    updated layout and styling, changed some text content

--- components/about.js
+++ components/about.js
@@ -3,31 +3,28 @@ import styles from "../styles/About.module.css"
 export default function About() {
     return (
         <div className={styles.aboutSection}>
-            <div className={styles.aboutHeadersAndImageDiv}>
-                <div className={styles.aboutHeaders}>
+            <div className={styles.aboutContent}>
+                <div className={styles.aboutText}>
                     <h1>
                         Jonathan King
-                    </h1>
+                </h1>
                     <h2>
                         Full-stack web developer with a passion
-                        <br />
-                        for user friendly designs
-                    </h2>
+                    <br />
+                    for user friendly designs
+                </h2>
+                    <div>
+                        <p>
+                            Life long learner, adept problem solver,
+                            experienced team player, customer service expert,
+                            skilled communicator, and enthusiastic gamer of
+                            nearly every flavor.
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div className={styles.aboutButtonsAndTextDiv}>
                 <div className={styles.aboutButtons}>
                     <button className={styles.aboutButton}>View my work</button>
                     <button className={styles.aboutButton}>Get in touch</button>
-                </div>
-                <div>
-                    <p>Life long learner, skilled problem solver,
-                    <br />
-                    communication and customer service expert,
-                    <br />
-                    experienced team player, strong work ethic,
-                    <br />
-                    and enthusiastic gamer of nearly every flavor.</p>
                 </div>
             </div>
         </div>
