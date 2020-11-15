49f9a8beda7dbbab10756b9c91d9d7d52362cdc2
Jonathan King <jonpking@gmail.com>
2020-11-14 23:29:02 -0700

    added contact info and image, css for sticky note layout for info and film layout for image

--- components/contact.js
+++ components/contact.js
@@ -3,7 +3,22 @@ import styles from "../styles/Contact.module.css"
 export default function Contact() {
     return (
         <div className={styles.contactSection}>
-
+            <div className={styles.contactCard}>
+                <div className={styles.contactTitle}>
+                    <h1>Jonathan King</h1>
+                    <h2>Full-Stack Web Developer</h2>
+                </div>
+                <hr className={styles.hr} />
+                <div className={styles.contactInfo}>
+                    <p><i className={`fas fa-envelope-square`}></i>jonpking@gmail.com</p>
+                    <p><i className={`fas fa-phone-square-alt`}></i>(602)703-1741</p>
+                    <p><i className={`${styles.linkedinIcon} fab fa-linkedin`}></i>linkedin.com/in/jonpking314</p>
+                    <p><i className={`${styles.githubIcon} fab fa-github-square`}></i>github.com/jonpking</p>
+                </div>
+            </div>
+            <div className={styles.imageCard}>
+                <img src="./images/self.jpg" />
+            </div>
         </div>
     )
 }
