c2ff197613f4897c4ff0f32d4be29061f63d74ef
Jonathan King <jonpking@gmail.com>
2020-11-23 13:16:58 -0700

    modified project and projects layout

--- components/projects.js
+++ components/projects.js
@@ -4,10 +4,16 @@ import Project from "../components/project.js"
 export default function Projects() {
     return (
         <div id="projects" className={styles.projectsSection}>
-            <Project />
-            <Project />
-            <Project />
-            <Project />
+            <div className={styles.projectsCardWrapper}>
+                <Project />
+                <Project />
+                <Project />
+                <Project />
+                <Project />
+                <Project />
+                <Project />
+                <Project />
+            </div>
         </div>
     )
 }
