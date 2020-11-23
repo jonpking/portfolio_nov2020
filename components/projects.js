b00d2e84755a0e34fc7037f21a87050db733e8a4
Jonathan King <jonpking@gmail.com>
2020-11-23 13:09:40 -0700

    refactored projects component and created project component to make section and card two seperate components

--- components/projects.js
+++ components/projects.js
@@ -1,31 +1,13 @@
 import styles from "../styles/Projects.module.css"
+import Project from "../components/project.js"
 
 export default function Projects() {
     return (
         <div id="projects" className={styles.projectsSection}>
-            <div className={styles.projectCard}>
-                <div className={styles.projectTitle}>
-                    <h3>Crystal Collector</h3>
-                </div>
-                <div className={styles.projectImage}>
-                    <img src="/images/computer.jpg"></img>
-                </div>
-                <div className={styles.projectDescription}>
-                    <p>A very simple number game built using onClick functions
-                    and jQuery. This was put together in approximately one
-                    week.</p>
-                </div>
-                <div className={styles.projectLinks}>
-                    <a href="">
-                        <i className="fab fa-lg fa-github"></i>
-                    View Repo
-                    </a>
-                    <a href="">
-                        <i className="fas fa-lg fa-link"></i>
-                    Live Demo
-                    </a>
-                </div>
-            </div>
+            <Project />
+            <Project />
+            <Project />
+            <Project />
         </div>
     )
 }
